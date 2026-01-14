3b79b3cdda3e58d8ecccc02299b7775868438881
Sara <ya96am77@gmail.com>
2026-01-14 06:33:52 +0300

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import ParentCorner from "./pages/ParentCorner";
 import NotFound from "./pages/NotFound";
 import SunnahLab from "./pages/SunnahLab";
 
+// 1. استيراد المكون الجديد (تأكد من إنشاء ملف LiquidEther.tsx في مجلد components)
+import LiquidEther from './components/LiquidEther'; 
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,16 +19,35 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/learn" element={<QuranLab />} />
-          <Route path="/parent" element={<ParentCorner />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/sunnah" element={<SunnahLab />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      
+      {/* 2. الحاوية الرئيسية للموقع لضمان ثبات الخلفية */}
+      <div className="relative min-h-screen">
+        
+        {/* 3. إضافة طبقة الخلفية المتحركة خلف كل شيء (-z-10) */}
+        <div className="fixed inset-0 -z-10 pointer-events-none">
+          <LiquidEther
+            colors={['#FDE68A', '#F97316', '#FB923C']} // ألوان سراج
+            mouseForce={15}
+            cursorSize={80}
+            resolution={0.4}
+            autoDemo={true}
+          />
+        </div>
+
+        {/* 4. محتوى الموقع الأصلي (الراوتر) في طبقة أعلى (z-10) */}
+        <div className="relative z-10">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/learn" element={<QuranLab />} />
+              <Route path="/parent" element={<ParentCorner />} />
+              <Route path="/sunnah" element={<SunnahLab />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+        
+      </div>
     </TooltipProvider>
   </QueryClientProvider>
 );
